1e0401d89326299813545752bb56412305d20550
roudai <me@roudai.net>
2020-04-23 11:34:44 +0900

    金額抽出を追加

--- onlyfive_diff.sh
+++ onlyfive_diff.sh
@@ -18,7 +18,8 @@ do
   name=$(echo $source | grep -oP '<title>.*</title>' | sed -e 's#<title>\(.*\)</title>#\1#' -e 's/ | Only Five//') 
   birth=$(echo $source | grep -oP '"profile-birthday-text">.*?</h3>' | sed -e 's#"profile-birthday-text">\(.*\)</h3>#\1#' -e 's/誕生日：//') 
   profile=$(echo $source | grep -oP '"creator-bio">.*?</div>' | sed -e 's#"creator-bio">\(.*\)</div>#\1#') 
-  echo $i,$name,$birth,'"'$profile'"' >> ${FILENAME}
+  price=$(echo $source | grep -oP '"post-price">.*?</div>' | sed -e 's#"post-price">\(.*\)</div>#\1#') 
+  echo $i,$name,$birth,'"'$profile'"','"'$price'"' >> ${FILENAME}
   i=`expr $i + 1`
 done
 echo $(date) "END" | tee -a $LOGFILE 
